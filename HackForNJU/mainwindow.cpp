33e19699d0375caecbf110312be069c52e2af076
SToPire <46126592+SToPire@users.noreply.github.com>
2019-03-12 00:26:49 +0800

    3-12.0:27

---
+++ HackForNJU/mainwindow.cpp
@@ -0,0 +1,75 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+vector<course> courses;
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(1,QHeaderView::ResizeToContents);
+    ui->tableWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+    ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ::courses=loadfile();
+    for(int i=0;i<courses.size();++i)
+    {
+        add_item(i,courses[i]);
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+vector<course> MainWindow::loadfile()
+{
+    vector<course> courses;
+    QFile coursefile("course.txt");
+    if (!coursefile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QMessageBox::critical(nullptr, "Wrong", "\"course.txt\" is not found.\n", QMessageBox::Yes);
+        return courses;
+    }
+    QTextStream in(&coursefile);
+    in.setCodec("GBK");
+    while(!in.atEnd())
+    {
+        course temp;
+        QString line=in.readLine(); if(line.isEmpty()) break;
+        temp.num=line.section(' ',0,0);
+        temp.name=line.section(' ',1,1);
+        temp.credit=line.section(' ',2,2).toInt();
+        temp.score=line.section(' ',3).toInt();
+        courses.push_back(temp);
+    }
+    coursefile.close();
+    return courses;
+}
+void MainWindow::add_item(int i,course info)
+{
+    ui->tableWidget->insertRow(i);
+    ui->tableWidget->setItem(i,0,new QTableWidgetItem(info.num));
+    ui->tableWidget->setItem(i,1,new QTableWidgetItem(info.name));
+    ui->tableWidget->setItem(i,2,new QTableWidgetItem(QString::number(info.credit)));
+    ui->tableWidget->setItem(i,3,new QTableWidgetItem(QString::number(info.score)));
+}
+void MainWindow::on_Calculate_clicked()
+{
+    QItemSelectionModel *selections=ui->tableWidget->selectionModel();
+    QModelIndexList selectedItems=selections->selectedIndexes();
+    set<int> si;
+    for(int i=0;i<selectedItems.count();++i)
+    {
+        si.insert(selectedItems.at(i).row());
+    }
+    double sum=0.0;
+    double cre=0.0;
+    for(auto a:si)
+    {
+        sum+=courses[a].credit*courses[a].score;
+        cre+=courses[a].credit;
+    }
+    ui->showGPA->setText(QString::number((sum/cre)/20.0,10,3));
+}
---
+++ build-HackForNJU-Desktop_Qt_5_11_2_MSVC2017_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,103 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QTableWidget *tableWidget;
+    QPushButton *Calculate;
+    QLabel *showGPA;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1046, 731);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        tableWidget = new QTableWidget(centralWidget);
+        if (tableWidget->columnCount() < 4)
+            tableWidget->setColumnCount(4);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(3, __qtablewidgetitem3);
+        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+        tableWidget->setGeometry(QRect(0, 310, 591, 351));
+        Calculate = new QPushButton(centralWidget);
+        Calculate->setObjectName(QStringLiteral("Calculate"));
+        Calculate->setGeometry(QRect(740, 240, 93, 28));
+        showGPA = new QLabel(centralWidget);
+        showGPA->setObjectName(QStringLiteral("showGPA"));
+        showGPA->setGeometry(QRect(230, 180, 72, 15));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 1046, 26));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\350\257\276\347\250\213\345\217\267", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "\350\257\276\347\250\213\345\220\215", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "\345\255\246\345\210\206", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(3);
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "\346\210\220\347\273\251", nullptr));
+        Calculate->setText(QApplication::translate("MainWindow", "\350\256\241\347\256\227", nullptr));
+        showGPA->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
