db0213dbe93c35d3f48838125eca68107aca968c
SToPire <865876164@qq.com>
2019-03-16 22:04:59 +0800

    final

--- HackForNJU/mainwindow.cpp
+++ HackForNJU/mainwindow.cpp
@@ -1,5 +1,7 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QString>
 vector<course> courses;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,11 +14,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->tableWidget->setSelectionMode(QAbstractItemView::MultiSelection);
     ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
     ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    ::courses=loadfile();
-    for(int i=0;i<courses.size();++i)
-    {
-        add_item(i,courses[i]);
-    }
     ui->nju->setChecked(true);
 }
 
@@ -24,16 +21,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-vector<course> MainWindow::loadfile()
+vector<course> MainWindow::loadfile(FILE *fp)
 {
     vector<course> courses;
-    QFile coursefile("course.txt");
-    if (!coursefile.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        QMessageBox::critical(nullptr, "Wrong", "\"course.txt\" is not found.\n", QMessageBox::Yes);
-        return courses;
-    }
-    QTextStream in(&coursefile);
+    QTextStream in(fp);
     in.setCodec("GBK");
     while(!in.atEnd())
     {
@@ -45,7 +36,7 @@ vector<course> MainWindow::loadfile()
         temp.score=line.section(' ',3).toInt();
         courses.push_back(temp);
     }
-    coursefile.close();
+    fclose(fp);
     return courses;
 }
 void MainWindow::add_item(int i,course info)
@@ -74,12 +65,104 @@ void MainWindow::on_Calculate_clicked()
             sum+=courses[a].credit*courses[a].score;
             cre+=courses[a].credit;
         }
-        ui->showGPA->setText(QString::number((sum/cre)/20.0,10,3));
+        ui->showGPA->setText(QString::number((sum/cre)/20.0,10,3)+"/5");
+    }
+    if(ui->std->isChecked())
+    {
+        set<int> si;
+        for(int i=0;i<selectedItems.count();++i)
+        {
+            si.insert(selectedItems.at(i).row());
+        }
+        double sum=0.0;
+        double cre=0.0;
+        for(auto a:si)
+        {
+            if(courses[a].score>=90){
+                sum+=4*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else if(courses[a].score>=80){
+                sum+=3*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else if(courses[a].score>=70){
+                sum+=2*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else if(courses[a].score>=60){
+                sum+=courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else{
+                cre+=courses[a].credit;
+            }
+        }
+        ui->showGPA->setText(QString::number((sum/cre),10,3)+"/4");
+    }
+    if(ui->wes->isChecked())
+    {
+        set<int> si;
+        for(int i=0;i<selectedItems.count();++i)
+        {
+            si.insert(selectedItems.at(i).row());
+        }
+        double sum=0.0;
+        double cre=0.0;
+        for(auto a:si)
+        {
+            if(courses[a].score>=85){
+                sum+=4*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else if(courses[a].score>=75){
+                sum+=3*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else if(courses[a].score>=60){
+                sum+=2*courses[a].credit;
+                cre+=courses[a].credit;
+            }
+            else {
+                sum+=courses[a].credit;
+                cre+=courses[a].credit;
+            }
+        }
+        ui->showGPA->setText(QString::number((sum/cre),10,3)+"/4");
     }
 }
 
 void MainWindow::on_selectAll_clicked()
 {
+    ui->tableWidget->selectAll();
+}
+
+void MainWindow::on_actionOpen_O_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,QString::fromLocal8Bit("请选择文件"),
+                                                     "D:",QString::fromLocal8Bit("文本文件(*txt *tsv)"));
+    FILE *fp;
+     fp=fopen(fileName.toStdString().c_str(), "r");
+     ::courses=loadfile(fp);
+     for(int i=0;i<courses.size();++i)
+     {
+         add_item(i,courses[i]);
+     }
+}
+
+void MainWindow::on_actionDeveloper_D_triggered()
+{
+    QMessageBox::information(this, QString::fromLocal8Bit("关于本软件"), QString::fromLocal8Bit("本软件由赵逸凡、张子辰和肖庚洋合作完成\n"));
+}
+
+void MainWindow::on_actionInstructions_I_triggered()
+{
+    QMessageBox::information(this, QString::fromLocal8Bit("使用方法"), QString::fromLocal8Bit("请将成绩存入文本文件，并选择相关计算方法进行计算\n"));
+}
+
+void MainWindow::on_selectAll_2_clicked()
+{
+    ui->tableWidget->selectAll();
     int row=ui->tableWidget->rowCount();
     for(int i=0;i<row;++i)
         ui->tableWidget->selectRow(i);
--- HackForNJU/mainwindow.h
+++ HackForNJU/mainwindow.h
@@ -34,9 +34,17 @@ public:
 
 private slots:
     void on_Calculate_clicked();
-    vector<course> loadfile();
+    vector<course> loadfile(FILE *fp);
     void add_item(int i,course info);
     void on_selectAll_clicked();
+
+    void on_actionOpen_O_triggered();
+
+    void on_actionDeveloper_D_triggered();
+
+    void on_actionInstructions_I_triggered();
+
+    void on_selectAll_2_clicked();
 
 private:
     Ui::MainWindow *ui;
