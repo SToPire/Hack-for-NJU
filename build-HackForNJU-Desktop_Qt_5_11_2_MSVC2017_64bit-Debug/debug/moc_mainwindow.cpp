7e1954f1b0cbbba3a911ad6f907e26cb5a3c7f78
SToPire <46126592+SToPire@users.noreply.github.com>
2019-03-16 21:18:53 +0800

    1

--- build-HackForNJU-Desktop_Qt_5_11_2_MSVC2017_64bit-Debug/debug/moc_mainwindow.cpp
+++ build-HackForNJU-Desktop_Qt_5_11_2_MSVC2017_64bit-Debug/debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../HackForNJU/mainwindow.h"
+#include "../../../../../Desktop/Hack-for-NJU-masterr/HackForNJU/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
